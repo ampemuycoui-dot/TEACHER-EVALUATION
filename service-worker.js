1fd89a1fb94e632c6a04bd9ec8c865bd8733909e
ampemuycoui-dot <ampe.muyco.ui@phinmaed.com>
2026-03-15 00:16:01 +0800

    Delete service-worker.js
    Yeyeue

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", function(e) {
-  e.waitUntil(
-    caches.open("Teacher Evaluation-cache").then(function(cache) {
-      return cache.addAll(["index.html"]);
-    })
-  );
-});
